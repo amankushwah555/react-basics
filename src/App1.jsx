4ac6b79e5bbb120a44641d0a55f60b134b3a4f41
Sujata <sujatamore660@gmail.com>
2024-06-06 17:53:14 +0530

    component App is of task1 and App1 is of task2

---
+++ src/App1.jsx
@@ -0,0 +1,71 @@
+// 
+import React, { useState } from 'react';
+
+const SmartPhoneBrandList = [
+    { label: "iPhone", value: "iPhone" },
+    { label: "Samsung", value: "Samsung" },
+    { label: "Huawei", value: "Huawei" },
+    { label: "Google Pixel", value: "Google Pixel" },
+    { label: "OnePlus", value: "OnePlus" },
+    { label: "Realme", value: "Realme" },
+    { label: "Readme", value: "Readme" },
+    { label: "LG", value: "LG" },
+    { label: "Motorola", value: "Motorola" },
+    { label: "Moka", value: "Moka" },
+];
+
+function App1() {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredBrands = searchTerm === '' ? [] : SmartPhoneBrandList.filter((brand) =>
+        brand.label.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px',marginLeft:"600px" }}>
+            <h1 
+                style={{
+                    color: 'pink',
+                    fontFamily: 'cursive',
+                    fontSize: '50px'
+                }}
+            >
+                Enter the required item
+            </h1>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                placeholder="Search for a brand..."
+                style={{
+                    fontSize: '30px',
+                    padding: '10px',
+                    width: '50%',
+                    margin: '20px 0'
+                }}
+            />
+            {filteredBrands.length > 0 && (
+                <ul style={{ listStyleType: 'none', padding: '0' }}>
+                    {filteredBrands.map((brand) => (
+                        <li 
+                            key={brand.value}
+                            style={{
+                                fontSize: '25px',
+                                padding: '10px 0',
+                                borderBottom: '1px solid #ccc'
+                            }}
+                        >
+                            {brand.label}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default App1;
