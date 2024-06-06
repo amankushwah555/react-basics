72533b9ddd0472fb596efaabdacc0a5d57975b4d
Sujata <sujatamore660@gmail.com>
2024-06-06 14:48:30 +0530

    task-1 module

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ color:'white',align:'center', paddingBottom:'300px',paddingLeft:'600px'}}>
+      <h1>Task:01</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        style={{ color:'black',backgroundColor:'pink',align:"center" ,padding: '10px', fontSize: '20px',fontWeight:'bold' }}
+      />
+      <p style={{ marginTop: '20px',marginLeft:'10px', fontSize: '20px',fontWeight:"bold" }}>{inputValue}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
